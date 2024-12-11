72b0996aea5e52eff92c9f3f55bb4bafff54a41b
unknown <wesleyfreire1707@gmail.com>
2024-12-11 20:45:25 -0300

    criando rotas

--- mai.py
+++ mai.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, url_for
 
+#inicializacao
 app =Flask(__name__)
+
+
+#rota
+@app.route('/')
+def ola_mundo():
+    return "eae suave"
+
+@app.route('/sobre')
+def ola_mundo():
+    return "<"
+  
+#execucao
+app.run(debug=True)
