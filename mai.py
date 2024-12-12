26f0de74ec81861db32b4f92f51ad88fc6efa676
unknown <wesleyfreire1707@gmail.com>
2024-12-11 21:26:29 -0300

    criando o template

--- mai.py
+++ mai.py
@@ -7,11 +7,11 @@ app =Flask(__name__)
 #rota
 @app.route('/')
 def ola_mundo():
-    return "eae suave"
+    return f"<a href='{url_for('pagina_sobre')}'>pagina sobre<a>"
 
 @app.route('/sobre')
-def ola_mundo():
-    return "<"
+def pagina_sobre():
+    return "<b> olha esse link<b>"
   
 #execucao
 app.run(debug=True)
